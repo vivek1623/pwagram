65537c8e6cffd3e30865622c3818f8da1877c59c
Vivek Vaibhav <vivek@oriserve.com>
2020-07-06 22:57:40 +0530

    initial commit

---
+++ public/sw.js
@@ -0,0 +1,38 @@
+
+self.addEventListener('install', event => {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open('static').then(cache => {
+      console.log('[Service Worker] precaching App Shell');
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/material.min.js',
+        '/src/js/fetch.js',
+        '/src/js/promise.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+      ])
+    })
+  )
+});
+
+self.addEventListener('activate', function (event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response
+      return (fetch(event.request))
+    })
+  );
+});
